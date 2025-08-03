7a8c7ea02da7293e6ba40e543f081b4eef744e22
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-03 22:47:12 +0300

    feat: setup database

---
+++ database/migrations/2025_08_03_194625_create_laporan_berkalas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan_berkalas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('perguruan_tinggi_id')->constrained('perguruan_tinggi');
+            $table->string('judul_laporan');
+            $table->enum('jenis_laporan', ['bulanan', 'semesteran', 'tahunan', 'khusus']);
+            $table->enum('periode', ['ganjil', 'genap', 'tahunan']);
+            $table->year('tahun_akademik');
+            $table->json('data_laporan'); // Ringkasan data dalam JSON
+            $table->string('file_laporan')->nullable(); // Path file PDF
+            $table->enum('status', ['draft', 'review', 'final']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan_berkalas');
+    }
+};
